8d4cc3f265bbdf1a8b8fa5d2a69ae639d42dde30
ni3rav <mahetan35@gmail.com>
2024-07-13 23:57:36 +0530

    mvp of code preview and used icons instead of text

--- lib/components/CodePreview/CodePreview.tsx
+++ lib/components/CodePreview/CodePreview.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Clipboard, Check } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
-
+import { Code, MonitorPlay } from "lucide-react";
 interface CodePreviewProps {
   component: React.ReactNode;
   code: string;
@@ -17,48 +17,48 @@ const CodePreview: React.FC<CodePreviewProps> = ({ component, code }) => {
     setTimeout(() => setCopied(false), 2000);
   };
 
+  // TODO: ANIMATION
+  // TODO: MAKE ICONS MORE DULL
   return (
     <div className="w-full h-full">
-      <div className="flex">
+      <div className="flex mb-2">
+        <div className="flex bg-red-500 rounded-md">
         <button
-          className={`px-4 py-2 ${
-            !showCode ? "bg-zinc-800 text-white" : "bg-zinc-700 text-gray-400"
-          }`}
+          className={`px-4 py-2  ${!showCode ? "text-white" : "text-gray-400"}`}
           onClick={() => setShowCode(false)}
         >
-          Preview
+          <MonitorPlay size={16} />
         </button>
         <button
-          className={`px-4 py-2 ${
-            showCode ? "bg-zinc-800 text-white" : "bg-zinc-700 text-gray-400"
-          }`}
+          className={`px-4 py-2 ${showCode ? "text-white" : "text-gray-400"}`}
           onClick={() => setShowCode(true)}
         >
-          Code
+          <Code size={16} />
         </button>
+        </div>
         {showCode && (
           <button
-            className="px-4 py-2 bg-zinc-800 text-white"
+            className=" text-white bg-red-400 rounded-md p-2"
             onClick={handleCopy}
           >
             <AnimatePresence>
               {copied ? (
                 <motion.div
                   key="check"
-                  initial={{ opacity: 0, y: 20 }}
+                  initial={{ opacity: 0, y: 1 }}
                   animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: 20 }}
-                  transition={{ duration: 0.3 }}
+                  exit={{ opacity: 0, y: 1 }}
+                  transition={{ duration: 0.15 }}
                 >
                   <Check className="h-4 w-4 text-green-500" />
                 </motion.div>
               ) : (
                 <motion.div
                   key="clipboard"
-                  initial={{ opacity: 0, y: 20 }}
+                  initial={{ opacity: 0, y: 1 }}
                   animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: 20 }}
-                  transition={{ duration: 0.3 }}
+                  exit={{ opacity: 0, y: 1 }}
+                  transition={{ duration: 0.15 }}
                 >
                   <Clipboard className="h-4 w-4" />
                 </motion.div>
@@ -67,7 +67,7 @@ const CodePreview: React.FC<CodePreviewProps> = ({ component, code }) => {
           </button>
         )}
       </div>
-      <div className="p-8 bg-zinc-950 text-white h-full w-full">
+      <div className="p-6 bg-zinc-950 text-white h-full w-full rounded-md">
         {showCode ? (
           <pre className="overflow-auto h-full">{code}</pre>
         ) : (
