c0ed182ae0d4632730f899f2d61ed7deb9a43430
Vedant Panchal <VEDANTPANCHAL.ict22@adaniuni.ac.in>
2024-07-16 18:34:08 +0530

    Updated docs theme along with codepreview component and changed header and footer of docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,4 +1,55 @@
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import dynamic from "next/dynamic";
+import Footer from "./components/ui/Footer/Footer";
+import { ReactNode } from "react";
+import Link from "next/link"; // Use next/link for navigation
+import { Component } from "lucide-react";
+import Head from "next/head";
+
+const CustomHead = () => {
+  const { asPath, defaultLocale, locale } = useRouter();
+  const { frontMatter } = useConfig();
+  const url =
+    "https://localhost:3000/" +
+    (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+  return (
+    <>
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={frontMatter.title || "ProtoUI"} />
+      <meta
+        property="og:description"
+        content={
+          frontMatter.description ||
+          "The next gen component library for React and Next JS"
+        }
+      />
+    </>
+  );
+};
+
+const Navbar = () => {
+  const { search } = useConfig();
+  const SearchComponent: any = search?.component;
+  return (
+    <header className="fixed z-50 top-0 flex h-max w-full items-center justify-between px-32 py-4 backdrop-blur-xl bg-[rgb(77,77,77)]/[0.1%] after:content-[''] after:absolute after:w-full after:h-[0.5px] after:bg-gradient-to-r from-[#787878]/0 via-[#DEDEDE] to-[#787878]/0 after:bottom-0 after:right-0 after:opacity-50">
+      <Link href="/" className="flex items-center space-x-2">
+        <div>
+          <span className="bg-gradient-to-br from-[#FFF] to-[#999] bg-clip-text font-fira text-2xl font-bold text-transparent">
+            {"<"}Proto{">"}
+          </span>
+          <span className="bg-gradient-to-r from-[#FB7185] to-[#F43F5E] bg-clip-text font-sans text-xl font-black text-transparent">
+            UI
+          </span>
+        </div>
+      </Link>
+      <div className="flex-grow flex justify-end items-center">
+        <SearchComponent />
+      </div>
+    </header>
+  );
+};
 
 const themeConfig: DocsThemeConfig = {
   logo: (
@@ -11,25 +62,60 @@ const themeConfig: DocsThemeConfig = {
       </span>
     </p>
   ),
-  project: {
-    link: "https://github.com/shuding/nextra",
-  },
   sidebar: {
     titleComponent({ title, type }) {
       if (type === "separator") {
-        return <span className="cursor-default">{title}</span>;
+        return (
+          <div style={{ background: "transparent", textAlign: "left" }}>
+            {title}
+          </div>
+        );
       }
-      return <>{title}</>;
+      return (
+        <div className="flex items-center justify-between gap-3">
+          <Component strokeWidth={2} size={15} /> {title}
+        </div>
+      );
     },
     defaultMenuCollapseLevel: 1,
-    toggleButton: true,
+    toggleButton: false,
+    autoCollapse: false,
   },
-  navbar: {},
-  banner: {
-    dismissible: false,
-    text: "Welcome ProtoUI 🚀🚀",
+  navbar: {
+    component: Navbar,
   },
-  
+  head: CustomHead,
+  darkMode: false,
+  footer: {
+    component: Footer,
+  },
+  toc: {
+    float: false,
+    backToTop: true,
+    component: null,
+  },
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: null,
+  },
+  main: ({ children }: { children: ReactNode }) => {
+    return (
+      <div className="flex flex-col items-start justify-start min-h-screen bg-transparent text-white ">
+        <div className="flex-grow max-w-full overflow-auto relative w-full">
+          {children}
+        </div>
+      </div>
+    );
+  },
+  useNextSeoProps: () => {
+    return {
+      titleTemplate: "%s | ProtoUI",
+    };
+  },
+  primaryHue: 332,
+  primarySaturation: 90,
 };
 
 export default themeConfig;
