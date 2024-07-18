3963491b6c457d05501c5691b649aedb58e19b58
Vedant Panchal <VEDANTPANCHAL.ict22@adaniuni.ac.in>
2024-07-18 18:57:47 +0530

    Delete utils directory

--- utils/cn.d.ts
+++
@@ -1,2 +0,0 @@
-import { ClassValue } from "clsx";
-export declare const cn: (...classes: ClassValue[]) => string;
--- utils/cn.js
+++
@@ -1,9 +0,0 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-export var cn = function () {
-    var classes = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        classes[_i] = arguments[_i];
-    }
-    return twMerge(clsx(classes));
-};
