77b98eeaa9b5a6ecef4b2b4a8358c44f14199277
Vedant Panchal <VEDANTPANCHAL.ict22@adaniuni.ac.in>
2024-07-23 00:32:28 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,16 @@ const config = {
         "ghost-text": "ghost-text",
         "ghost-hover": "ghost-hover",
       },
+backgroundImage: {
+        linear:
+          "linear-gradient(180deg, #1a1a1a 0%, #1c1a1a 50%, #0e0e0e 100%)",
+        noise:"url('/noise.svg')"
+      },
+      fontFamily: {
+        fira: ["Fira Code", "monospace"],
+        montserrat: ["Montserrat", "sans-serif"],
+        geist: ['var(--font-geist-sans)'],
+},
     },
   },
 } satisfies Config
