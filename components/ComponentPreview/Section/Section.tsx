7cdc018179ddfb55878b95b751eaef3983219328
ni3rav <mahetan35@gmail.com>
2024-07-30 22:42:41 +0530

    Revert "removed redundant code from sidebar and section"

--- components/ComponentPreview/Section/Section.tsx
+++ components/ComponentPreview/Section/Section.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
-import Sidebar from "./Sidebar";
-import CardDisplay from "./CardDisplay";
-import { ComponentCard } from "@/components/ui/ComponentCards/ComponentCard";
-import { ChevronRight, Gift, SquareArrowOutUpRight } from "lucide-react";
+import React, { useState } from 'react';
+import Sidebar from './Sidebar';
+import CardDisplay from './CardDisplay';
+import { ComponentCard } from '@/components/ui/ComponentCards/ComponentCard';
+import { ChevronRight, Gift, SquareArrowOutUpRight } from 'lucide-react';
 import {
   Accordion,
   AccordionItem,
@@ -15,16 +15,20 @@ import Alert from "@components/Alert/Alert";
 import AlertTitle from "@components/Alert/AlertTitle";
 import AlertDescription from "@components/Alert/AlertDescription";
 import Avatar from "@components/Avatar/Avatar";
-import Button from "@components/Button/Button";
+import Button from '@components/Button/Button';
 import {
   Card,
   CardHeader,
   CardTitle,
   CardFooter,
+  CardButton,
   CardBody,
   CardContainer,
   CardIcon,
+  CardImage,
   CardLink,
+  CardCustomer,
+  CardRating,
 } from "@components/Card/Card";
 import Checkbox from "@components/Checkbox/Checkbox";
 import {
@@ -34,11 +38,11 @@ import {
   DropdownContent,
   DropdownLabel,
 } from "@components/Dropdown/Dropdown";
-import { Input } from "@components/Input/Input";
+import { Input } from '@components/Input/Input';
 import Radio from "@components/Radio/Radio";
 import { TextArea } from "@components/TextArea/TextArea";
 import { Toggle } from "@components/Toggle/Toggle";
-import ToggleSwitch from "./ToggleSwitch";
+import ToggleSwitch from './ToggleSwitch';
 
 const Section: React.FC = () => {
   const [sidebarVisible, setSidebarVisible] = useState(false);
@@ -47,190 +51,197 @@ const Section: React.FC = () => {
     setSidebarVisible(checked);
   };
 
-  const components = [
-    {
-      id: "Accordion",
-      title: "Accordion",
-      description: "An accordion component with expand/collapse functionality.",
-      buttonLink: "/docs/accordion",
-      content: (
-        <Accordion
-          variant="rounded"
-          border={true}
-          className="gap-2 w-full md:w-[400px]"
-        >
-          {Array.from({ length: 3 }, (_, index) => (
-            <AccordionItem key={index} index={index}>
-              <AccordionButton>Section {index + 1}</AccordionButton>
-              <AccordionContent>
-                Content for section {index + 1}
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
-      ),
-    },
-    {
-      id: "Card",
-      title: "Card",
-      description: "A Card component to display content.",
-      buttonLink: "/docs/card",
-      content: (
-        <ComponentCard>
-          <Card>
-            <CardContainer>
-              <CardHeader>
-                <CardIcon>
-                  <Gift className="h-10 w-10" />
-                </CardIcon>
-                <CardTitle>Lorem ipsum dolor sit amet</CardTitle>
-              </CardHeader>
-              <CardBody>
-                Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-                consectetur, adipisci velit...
-              </CardBody>
-              <CardFooter>
-                <CardLink
-                  href={"/backgrounds"}
-                  className="cursor-pointer flex flex-row items-center"
-                >
-                  <p>See your guidelines</p>
-                  <CardIcon>
-                    <SquareArrowOutUpRight className="ml-3 h-4 w-4 text-[#F65571] mt-2" />
-                  </CardIcon>
-                </CardLink>
-              </CardFooter>
-            </CardContainer>
-          </Card>
-        </ComponentCard>
-      ),
-    },
-    {
-      id: "Alert",
-      title: "Alert",
-      description: "An alert component to display messages.",
-      buttonLink: "/docs/alert",
-      content: (
-        <Alert dismissAfter={200000} className="w-full md:w-[400px]">
-          <AlertTitle>This is alert title!</AlertTitle>
-          <AlertDescription>This is alert description!</AlertDescription>
-        </Alert>
-      ),
-    },
-    {
-      id: "Avatar",
-      title: "Avatar",
-      description: "An avatar component to display user profile images.",
-      buttonLink: "/docs/avatar",
-      content: (
-        <>
-          <Avatar isRounded={false} location="top-right" />
-          <Avatar status="online" />
-        </>
-      ),
-    },
-    {
-      id: "Button",
-      title: "Button",
-      description: "A button component to trigger actions.",
-      buttonLink: "/docs/button",
-      content: <Button variant="success">Don't you dare</Button>,
-    },
-    {
-      id: "Checkbox",
-      title: "Checkbox",
-      description: "A checkbox component to select options.",
-      buttonLink: "/docs/checkbox",
-      content: <Checkbox label="Label" />,
-    },
-    {
-      id: "Dropdown",
-      title: "Dropdown",
-      description: "A dropdown component to select options.",
-      buttonLink: "/docs/dropdown",
-      content: (
-        <Dropdown>
-          <DropdownButton defaultText="Select an option" />
-          <DropdownContent>
-            <DropdownLabel>Options</DropdownLabel>
-            {["Option 1", "Option 2", "Option 3"].map((option, index) => (
-              <DropdownItem key={index} value={option}>
-                {option}
-              </DropdownItem>
-            ))}
-          </DropdownContent>
-        </Dropdown>
-      ),
-    },
-    {
-      id: "Input",
-      title: "Input",
-      description: "An input component to enter data.",
-      buttonLink: "/docs/input",
-      content: (
-        <Input
-          placeholder="Enter some text"
-          type="password"
-          showIcon={true}
-          variant="error"
-          className="input-field"
-        />
-      ),
-    },
-    {
-      id: "Radio",
-      title: "Radio",
-      description: "A radio component to select options.",
-      buttonLink: "/docs/radio",
-      content: <Radio label="Label" />,
-    },
-    {
-      id: "Textarea",
-      title: "Textarea",
-      description: "A textarea component to enter multiple lines of text.",
-      buttonLink: "/docs/textarea",
-      content: <TextArea placeholder="Enter your text here..." variant="" />,
-    },
-    {
-      id: "Toggle",
-      title: "Toggle",
-      description: "A toggle component to switch between two states.",
-      buttonLink: "/docs/toggle",
-      content: <Toggle label="Default Toggle" />,
-    },
-  ];
-
   return (
-    <div className="md:flex-row md:items-start md:justify-start flex flex-col items-start justify-start relative">
-      <div
-        className={`relative ${sidebarVisible ? "block" : "hidden"} md:block`}
-      >
+    <div className="flex mt-28">
+      <div className={`relative ${sidebarVisible ? 'block' : 'hidden'} md:block`}>
         <Sidebar />
       </div>
-      <div
-        className={`md:hidden block sticky top-[125px] w-max h-max z-[1000] ${sidebarVisible ? "left-1/2" : "left-0"}`}
-      >
-        <ToggleSwitch onChange={toggleSidebar} />
-      </div>
-      <div className="flex-grow p-0 text-white md:ml-[240px] pt-28">
-        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 md:w-full">
-          {components.map((component) => (
-            <div key={component.id} id={component.id} className="mb-6">
-              <CardDisplay
-                title={component.title}
-                description={component.description}
-                buttonText="Try Now"
-                buttonLink={component.buttonLink}
-                buttonIcon={<ChevronRight className="h-6 w-6" />}
-              >
-                {component.content}
-              </CardDisplay>
-            </div>
-          ))}
+      <div className="flex-grow p-6 text-white mr-8 md:ml-[240px] ml-6">
+        <div className="md:hidden absolute top-28 left-0 mt-8">
+          <ToggleSwitch 
+            onChange={toggleSidebar}
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6">
+          <div id="Accordion" className="mb-6">
+            <CardDisplay 
+              title="Accordion" 
+              description="An accordion component with expand/collapse functionality."
+              buttonText="Try Now"
+              buttonLink="/docs/accordion"
+              buttonIcon={<ChevronRight className="h-6 w-6" />}
+            >
+              <Accordion variant="rounded" border={true} className='gap-2 w-full md:w-[400px]'>
+                <AccordionItem index={0}>
+                  <AccordionButton>Section 1</AccordionButton>
+                  <AccordionContent>Content for section 1</AccordionContent>
+                </AccordionItem>
+                <AccordionItem index={1}>
+                  <AccordionButton>Section 2</AccordionButton>
+                  <AccordionContent>Content for section 2</AccordionContent>
+                </AccordionItem>
+                <AccordionItem index={2}>
+                  <AccordionButton>Section 3</AccordionButton>
+                  <AccordionContent>Content for section 3</AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </CardDisplay>
+          </div>
+          <div id='Card' className='mb-6'>
+            <CardDisplay
+              title='CardDisplay'
+              description='A CardDisplay component to display content.'
+              buttonText='Try Now'
+              buttonLink='/docs/card'
+              buttonIcon={<ChevronRight className="h-6 w-6" />}
+            >
+              <ComponentCard>
+                <Card>
+                  <CardContainer>
+                    <CardHeader>
+                      <CardIcon>
+                        <Gift className="h-10 w-10" />
+                      </CardIcon>
+                      <CardTitle>Lorem ipsum dolor sit amet</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                      Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+                      consectetur, adipisci velit...
+                    </CardBody>
+                    <CardFooter>
+                      <CardLink href={"/backgrounds"} className="cursor-pointer flex flex-row items-center">
+                        <p>See your guidelines</p>
+                        <CardIcon>
+                          <SquareArrowOutUpRight className="ml-3 h-4 w-4 text-[#F65571] mt-2" />
+                        </CardIcon>
+                      </CardLink>
+                    </CardFooter>
+                  </CardContainer>
+                </Card>
+              </ComponentCard>
+            </CardDisplay>
+          </div>
+          <div id='Alert' className='mb-6'>
+            <CardDisplay
+              title='Alert'
+              description='An alert component to display messages.'
+              buttonText='Try Now'
+              buttonLink='/docs/alert'
+              buttonIcon={<ChevronRight className="h-6 w-6" />}
+            >
+              <Alert dismissAfter={200000} className="w-full md:w-[400px]">
+                <AlertTitle>This is alert title!</AlertTitle>
+                <AlertDescription>This is alert description!</AlertDescription>
+              </Alert>
+            </CardDisplay>
+          </div>
+          <div id='Avatar' className='mb-6'>
+            <CardDisplay
+              title='Avatar'
+              description='An avatar component to display user profile images.'
+              buttonText='Try Now'
+              buttonLink='/docs/avatar'
+              buttonIcon={<ChevronRight className="h-6 w-6" />}
+            >
+              <Avatar isRounded={false} location="top-right" />
+              <Avatar status="online" />
+            </CardDisplay>
+          </div>
+          <div id='Button' className='mb-6'>
+            <CardDisplay
+              title='Button'
+              description='A button component to trigger actions.'
+              buttonText='Try Now'
+              buttonLink='/docs/button'
+              buttonIcon={<ChevronRight className="h-6 w-6" />}
+            >
+              <Button variant='success'>dont you dare</Button>
+            </CardDisplay>
+          </div>
+          <div id='Checkbox' className='mb-6'>
+            <CardDisplay
+              title='Checkbox'
+              description='A checkbox component to select options.'
+              buttonText='Try Now'
+              buttonLink='/docs/checkbox'
+              buttonIcon={<ChevronRight className="h-6 w-6" />}
+            >
+              <Checkbox label="Label" />
+            </CardDisplay>
+          </div>
+          <div id='Dropdown' className='mb-6'>
+            <CardDisplay
+              title='Dropdown'
+              description='A dropdown component to select options.'
+              buttonText='Try Now'
+              buttonLink='/docs/dropdown'
+              buttonIcon={<ChevronRight className="h-6 w-6" />}
+            >
+              <Dropdown>
+                <DropdownButton defaultText="Select an option" />
+                <DropdownContent>
+                  <DropdownLabel>Options</DropdownLabel>
+                  <DropdownItem value="Option 1">Option 1</DropdownItem>
+                  <DropdownItem value="Option 2">Option 2</DropdownItem>
+                  <DropdownItem value="Option 3">Option 3</DropdownItem>
+                </DropdownContent>
+              </Dropdown>
+            </CardDisplay>
+          </div>
+          <div id='Input' className='mb-6'>
+            <CardDisplay
+              title='Input'
+              description='An input component to enter data.'
+              buttonText='Try Now'
+              buttonLink='/docs/input'
+              buttonIcon={<ChevronRight className="h-6 w-6" />}
+            >
+              <Input
+                placeholder="Enter some text"
+                type="password"
+                showIcon={true}
+                variant="error"
+                className="input-field"
+              />
+            </CardDisplay>
+          </div>
+          <div id='Radio' className='mb-6'>
+            <CardDisplay
+              title='Radio'
+              description='A radio component to select options.'
+              buttonText='Try Now'
+              buttonLink='/docs/radio'
+              buttonIcon={<ChevronRight className="h-6 w-6" />}
+            >
+              <Radio label="Label" />
+            </CardDisplay>
+          </div>
+          <div id='Textarea' className='mb-6'>
+            <CardDisplay
+              title='Textarea'
+              description='A textarea component to enter multiple lines of text.'
+              buttonText='Try Now'
+              buttonLink='/docs/textarea'
+              buttonIcon={<ChevronRight className="h-6 w-6" />}
+            >
+              <TextArea placeholder="Enter your text here..." variant="" />
+            </CardDisplay>
+          </div>
+          <div id='Toggle' className='mb-6'>
+            <CardDisplay
+              title='Toggle'
+              description='A toggle component to switch between two states.'
+              buttonText='Try Now'
+              buttonLink='/docs/toggle'
+              buttonIcon={<ChevronRight className="h-6 w-6" />}
+            >
+              <Toggle label="Default Toggle" />
+            </CardDisplay>
+          </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Section;
--- components/ComponentPreview/Section/Sidebar.tsx
+++ components/ComponentPreview/Section/Sidebar.tsx
@@ -1,52 +1,74 @@
-import React, { useState } from "react";
-import Link from "next/link";
-import ToggleSwitch from "./ToggleSwitch";
-
-const components = [
-  { href: "/docs/accordion", label: "Accordion" },
-  { href: "/docs/alert", label: "Alert" },
-  { href: "/docs/avatar", label: "Avatar" },
-  { href: "/docs/backgrounds", label: "Backgrounds" },
-  { href: "/docs/button", label: "Button" },
-  { href: "/docs/card", label: "Card" },
-  { href: "/docs/checkbox", label: "Checkbox" },
-  { href: "/docs/dropdown", label: "Dropdown" },
-  { href: "/docs/input", label: "Input" },
-  { href: "/docs/radio", label: "Radio" },
-  { href: "/docs/textarea", label: "Textarea" },
-  { href: "/docs/toggle", label: "Toggle" },
-];
+import React from 'react';
+import Link from 'next/link';
 
 const Sidebar: React.FC = () => {
-  const [isExpanded, setIsExpanded] = useState(true);
-  const linkClasses =
-    "mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md";
-
-  const handleToggle = (checked: boolean) => {
-    setIsExpanded(checked);
-  };
-
   return (
-    <div
-      className={`fixed top-0 left-0 h-full overflow-y-auto p-4 z-10 text-white bg-black custom-scrollbar transition-all duration-300 ${
-        isExpanded ? "w-full md:w-64" : "w-16"
-      }`}
-    >
-      <div className="flex items-center justify-between mb-8 mt-[90px]">
-        {isExpanded && <h2 className="text-2xl font-bold">Components</h2>}
-        <ToggleSwitch onChange={handleToggle} />
+    <div className="fixed top-28 mt-6 mb-8 left-8 w-64 bottom-16 overflow-y-auto p-4 z-10 text-white custom-scrollbar">
+      <h2 className="text-2xl font-bold mb-8">All Components</h2>
+      <div className="flex-grow">
+        <ul>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/accordion">
+              Accordion
+            </Link>
+          </li>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/alert">
+              Alert
+            </Link>
+          </li>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/avatar">
+              Avatar
+            </Link>
+          </li>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/backgrounds">
+              Backgrounds
+            </Link>
+          </li>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/button">
+              Button
+            </Link>
+          </li>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/card">
+              Card
+            </Link>
+          </li>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/checkbox">
+              Checkbox
+            </Link>
+          </li>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/dropdown">
+              Dropdown
+            </Link>
+          </li>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/input">
+              Input
+            </Link>
+          </li>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/radio">
+              Radio
+            </Link>
+          </li>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/textarea">
+              Textarea
+            </Link>
+          </li>
+          <li className="mb-2 hover:text-[#F65571] hover:pl-4 transition-all p-2 rounded-md">
+            <Link href="/docs/toggle">
+              Toggle
+            </Link>
+          </li>
+        </ul>
       </div>
-      {isExpanded && (
-        <div className="flex-grow">
-          <ul>
-            {components.map((component) => (
-              <li key={component.href} className={linkClasses}>
-                <Link href={component.href}>{component.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
     </div>
   );
 };
