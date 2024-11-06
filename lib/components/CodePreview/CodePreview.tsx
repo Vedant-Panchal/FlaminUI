a8e7f4ca335cb90eebe4806a134a777a9ee5ffd3
ni3rav <mahetan35@gmail.com>
2024-11-06 17:41:27 +0530

    button checkbox and codepreview

--- lib/components/CodePreview/CodePreview.tsx
+++ lib/components/CodePreview/CodePreview.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Clipboard, Check } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Code, MonitorPlay } from "lucide-react";
+
 interface CodePreviewProps {
   component: React.ReactNode;
   code: string;
@@ -17,28 +18,30 @@ const CodePreview: React.FC<CodePreviewProps> = ({ component, code }) => {
     setTimeout(() => setCopied(false), 2000);
   };
 
-  // TODO: ANIMATION
-  // TODO: MAKE ICONS MORE DULL
   return (
     <div className="w-full h-full">
       <div className="flex mb-2">
         <div className="flex bg-red-500 rounded-md">
-        <button
-          className={`px-4 py-2  ${!showCode ? "text-white" : "text-gray-400"}`}
-          onClick={() => setShowCode(false)}
-        >
-          <MonitorPlay size={16} />
-        </button>
-        <button
-          className={`px-4 py-2 ${showCode ? "text-white" : "text-gray-400"}`}
-          onClick={() => setShowCode(true)}
-        >
-          <Code size={16} />
-        </button>
+          <button
+            className={
+              "px-4 py-2 " + (!showCode ? "text-white" : "text-gray-400")
+            }
+            onClick={() => setShowCode(false)}
+          >
+            <MonitorPlay size={16} />
+          </button>
+          <button
+            className={
+              "px-4 py-2 " + (showCode ? "text-white" : "text-gray-400")
+            }
+            onClick={() => setShowCode(true)}
+          >
+            <Code size={16} />
+          </button>
         </div>
         {showCode && (
           <button
-            className=" text-white bg-red-400 rounded-md p-2"
+            className="text-white bg-red-400 rounded-md p-2"
             onClick={handleCopy}
           >
             <AnimatePresence>
